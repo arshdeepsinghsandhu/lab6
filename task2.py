451d52c02de822fd27fbed48b1e89bfeb7478d9f
arshdeepsinghsandhu <arshsandhu@dal.ca>
2018-01-22 15:38:30 -0400

    new class called rectangle created

---
+++ task2.py
@@ -0,0 +1,12 @@
+class Point():
+    x = 0
+    y = 0
+class Rectangle(object):
+    width = 0
+    height = 0
+box = Rectangle()
+box.corner = Point()
+box.width = 10
+box.height = 20
+box.corner.x = 0.0
+box.corner.y = 0.0
