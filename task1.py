0535f2bf58afebc057f81eebb1278509a99090d0
arshdeepsinghsandhu <arshsandhu@dal.ca>
2018-01-22 14:41:57 -0400

    print distance between two points

--- task1.py
+++ task1.py
@@ -1,5 +1,15 @@
+#def distance_between_points(x,y)
+import math
 class Point(object):
     """represents the point in 2-D space"""
 blank = Point()
 blank.x = 3.0
 blank.y = 4.0
+x = blank.x
+y = blank.y
+distance = math.sqrt(x**2 + y**2)
+
+
+print (x,y)
+
+print (distance)
