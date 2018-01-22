ec624d68537f3f4fb5259f7ed8790499677c125a
arshdeepsinghsandhu <arshsandhu@dal.ca>
2018-01-22 15:53:28 -0400

    function find center created and printing the center of box

--- task2.py
+++ task2.py
@@ -10,3 +10,16 @@ box.width = 10
 box.height = 20
 box.corner.x = 0.0
 box.corner.y = 0.0
+
+def find_center(rect):
+    p = Point()
+    p.x = rect.corner.x + rect.width/2.0
+    p.y = rect.corner.y + rect.height/2.0
+    return p
+
+def print_point(p):
+    print (p.x , p.y)
+
+center = find_center(box)
+
+print_point(center)
