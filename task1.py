f02c6f7a3f08507cc2314e84e10fff293b6a7763
arshdeepsinghsandhu <arshsandhu@dal.ca>
2018-01-22 15:17:04 -0400

    function created to return distance between two numbers

--- task1.py
+++ task1.py
@@ -1,15 +1,18 @@
-#def distance_between_points(x,y)
 import math
-class Point(object):
+class Point():
     """represents the point in 2-D space"""
-blank = Point()
-blank.x = 3.0
-blank.y = 4.0
-x = blank.x
-y = blank.y
-distance = math.sqrt(x**2 + y**2)
+    x=0
+    y=0
+p1 = Point()
+p1.x = 3
+p1.y = 3
+p2 = Point()
+p2.x = 5
+p2.y = 5
+
+def distance_between_points(p1,p2):
+    distance = math.sqrt((p1.x-p2.x)**2 + (p1.y-p2.y)**2)
+    return (distance)
 
 
-print (x,y)
-
-print (distance)
+print(distance_between_points(p1,p2))
